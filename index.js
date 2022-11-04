e61803aef270bb244711e9ed2f451c1b54597e25
hoang0650 <hoangphan29091999@gmail.com>
2022-11-04 14:30:27 +0700

    update 3

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
 const ccxt = require('ccxt');
 const moment = require('moment');
-const _ = require('lodash')
-
 
 async function main(){
-
-
     const binance = new ccxt.binance({
         // apiKey:'UJs6zrG0grTWrXH4fjraiCG1J5uUhlSx0HYUdBXIEjoRIDAaAh6mYmAsIpOWzT6z',
         // secret:'ezYVLpr91e95p8PwUXBsGQB2qc46qWQgyMbU34ol5QvX61yTxvJlhpO1GVS7gXHd', 
     })
+    const high = [];
+    const low = [];
     const price = await binance.fetchOHLCV('BTC/USDT','1m', undefined,5);
     const bPrice = price.map(price=>{
+        high.push(price[2]);
+        low.push(price[3]);
         return{
             timestamp: moment(price[0]).format(),
             open: price[1], high: price[2], low: price[3], close: price[4], volume: price[5]
         }
     })
     console.log(bPrice);
-    // const averagePrice = bPrice.reduce((acc,price)=>
-    //     acc + price.close
-    // , 0)/5
-    const max = price.map(price=> price[2])
-    const findMax = _.max(max);
-
-    const min = price.map(price=>price[3])  
-    const findMin = _.min(min);
-
-    const tranformMax = parseFloat(findMax)
-    const tranformMin = parseFloat(findMin)
-    const change = tranformMax/tranformMin
-
-    const lastPrice = parseFloat(bPrice[bPrice.length - 1].close)
-    console.log('lastPrice:',lastPrice)
-    // console.log('%', (change -1)*100 >= 1)
-    // console.log("Short", ((tranformMin/lastPrice)-1)*100>=0.1)
-    // console.log("Long",((lastPrice/tranformMin)-1)*100>=0.1)
+    console.log("high",high);
+    console.log("low",low);
+    
+    if(high.length<=5){
+        max = Math.max.apply(null,high)
+    }
+    if(low.length<=5){
+        min = Math.max.apply(null,low);
+    }
+    const lastPrice = (bPrice[bPrice.length - 1].close)
+    const change = max/min
+    console.log("change",change);
     if((change-1)*100 >= 1){
-        if(((tranformMax/lastPrice)-1)*100 >= 0.1){
+        if(((max/lastPrice)-1)*100 >= 0.1){
             console.log("Bán đi chờ chi")
-        }if (((lastPrice/tranformMin)-1)*100 >= 0.1) {
+        }if (((lastPrice/min)-1)*100 >= 0.1) {
             console.log("Mua đi chờ chi")
         } else {
             console.log("Bình tĩnh chờ thời")
         }
     }
+     
 }
 
 main()
